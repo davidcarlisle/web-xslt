dc44d377584d3cea4fc3f992f33afc37ebc8d37b
d.p.carlisle <d.p.carlisle@38069634-eb5d-11de-a70e-4b51a9783938>
2010-01-21 21:56:27 +0000

    add

---
+++ trunk/mmlclipboard/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 41);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(564, 317);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check for MathML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 370);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "MathML Clipboard Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ trunk/mmlclipboard/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        IDataObject ido = new DataObject();
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+           
+            
+            private void button1_Click(object sender, System.EventArgs e)
+{
+   
+
+ ido = Clipboard.GetDataObject();
+            var data = (MemoryStream)ido.GetData("MathML");
+
+            if (data == null)
+            {
+                textBox2.Text = "No MathML on Clipboard";
+            }
+            else
+            {
+                string mathMl;
+                using (data)
+                {
+                    using (var sr = new StreamReader(data))
+                    {
+                        mathMl = sr.ReadToEnd();
+                    }
+                }
+
+                textBox2.Text = mathMl.Replace("><", ">\r\n<");
+            }
+          // good, for text
+            //textBox2.Text = (string)ido.GetData(DataFormats.UnicodeText);
+            }
+
+        }
+     
+    }
+
