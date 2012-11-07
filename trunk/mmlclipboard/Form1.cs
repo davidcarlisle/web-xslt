0e513d593886358008f85bee59276022e1fd6e6a
d.p.carlisle <d.p.carlisle@38069634-eb5d-11de-a70e-4b51a9783938>
2012-11-07 23:17:36 +0000

    text formats

--- trunk/mmlclipboard/Form1.cs
+++ trunk/mmlclipboard/Form1.cs
@@ -31,38 +31,48 @@ namespace WindowsFormsApplication1
             ido = Clipboard.GetDataObject();
 
 
-            var data = (MemoryStream)ido.GetData(comboBox1.Text);
-
-            if (data == null)
+            try
             {
-                textBox2.Text = "No \"" + comboBox1.Text + "\" on Clipboard\r\nAvailable formats:\r\n";
-                for (var i = 0; i < ido.GetFormats().Length; i++)
-                {
-                    textBox2.Text += ido.GetFormats()[i];
-                    textBox2.Text += "\r\n";
-                }
-
+                string datastr = ido.GetData(comboBox1.Text).ToString();
+                if (datastr == "System.IO.MemoryStream") throw new Exception();
+                textBox2.Text = datastr;
             }
-            else
+            catch
             {
-                string mathMl;
-                using (data)
+
+                var data = (MemoryStream)ido.GetData(comboBox1.Text);
+
+                if (data == null)
                 {
-                    using (var sr = new StreamReader(data))
+                    textBox2.Text = "No \"" + comboBox1.Text + "\" on Clipboard\r\nAvailable formats:\r\n";
+                    for (var i = 0; i < ido.GetFormats().Length; i++)
                     {
-                        mathMl = sr.ReadToEnd();
+                        textBox2.Text += ido.GetFormats()[i] + " " + ido.GetFormats()[i].GetType();
+                        textBox2.Text += "\r\n";
                     }
-                }
 
-                textBox2.Text = mathMl.Replace("><", ">\r\n<");
+                }
+                else
+                {
+                    string mathMl;
+                    using (data)
+                    {
+                        using (var sr = new StreamReader(data))
+                        {
+                            mathMl = sr.ReadToEnd();
+                        }
+                    }
+
+                    textBox2.Text = mathMl.Replace("><", ">\r\n<");
+                }
+                // good, for text
+                //textBox2.Text = (string)ido.GetData(DataFormats.UnicodeText);
             }
-            // good, for text
-            //textBox2.Text = (string)ido.GetData(DataFormats.UnicodeText);
         }
 
         private void button2_Click(object sender, System.EventArgs e)
         {
-            string str1 = "X<?xml version=\"1.0\" encoding=\"UTF-16\"?><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mfrac><mi>a</mi><mi>b</mi></mfrac></math>";
+            string str1 = "X<?xml version=\"1.0\" encoding=\"UTF-16\"?><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mfrac><mi>a</mi><mi>b</mi></mfrac></math>\r\n";
             Byte[] str2 = System.Text.Encoding.Unicode.GetBytes(str1);
             str2[0] = 255;
             str2[1] = 254;
@@ -76,7 +86,10 @@ namespace WindowsFormsApplication1
             XmlReader reader = XmlReader.Create(new StringReader(str0));
             try
             {
-                XDocument testdoc = new XDocument(XDocument.Parse(str0));
+                if (checkBox1.Checked)
+                {
+                    XDocument testdoc = new XDocument(XDocument.Parse(str0));
+                }
 
                 string str1 = Regex.Replace(str0, @"^(<\?xml.*\?>)?", "X<?xml version=\"1.0\" encoding=\"UTF-16\"?>");
                 Byte[] str2 = System.Text.Encoding.Unicode.GetBytes(str1);
@@ -89,6 +102,11 @@ namespace WindowsFormsApplication1
             {
                 textBox2.Text = "\r\nXML error Data not copied:\r\n\r\n" + xmlerr.Message + "\r\n-----\r\n" + textBox2.Text;
             }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
 
         }
 
