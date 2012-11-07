f784d46e898dd61d6ed4b6ea860f9f4b0d63327d
d.p.carlisle <d.p.carlisle@38069634-eb5d-11de-a70e-4b51a9783938>
2012-11-07 23:17:36 +0000

    text formats

--- mmlclipboard/Form1.Designer.cs
+++ mmlclipboard/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // textBox2
@@ -82,7 +83,9 @@
             "MathML",
             "MathML Presentation",
             "MathML Content",
-            "TeX"});
+            "TeX",
+            "UnicodeText",
+            "Text"});
             this.comboBox1.Location = new System.Drawing.Point(107, 5);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(138, 21);
@@ -98,11 +101,25 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Clipboard Flavor: ";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(380, 7);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Check XML";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 370);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
@@ -124,6 +141,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
