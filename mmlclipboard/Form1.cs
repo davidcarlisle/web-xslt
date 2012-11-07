f4f656af0ef415b17506ddfa49dda7de005bcbcb
d.p.carlisle <d.p.carlisle@38069634-eb5d-11de-a70e-4b51a9783938>
2012-11-07 14:52:04 +0000

    use Linq for XML parser to check XML well formed before placing on clipboard

--- mmlclipboard/Form1.cs
+++ mmlclipboard/Form1.cs
@@ -8,29 +8,31 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
         IDataObject ido = new DataObject();
-      
+
 
         public Form1()
         {
             InitializeComponent();
         }
-           
-            
-            private void button1_Click(object sender, System.EventArgs e)
-{
-   
 
- ido = Clipboard.GetDataObject();
 
-             
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
+
+            ido = Clipboard.GetDataObject();
+
+
             var data = (MemoryStream)ido.GetData("MathML");
-               
+
             if (data == null)
             {
                 textBox2.Text = "No MathML on Clipboard\r\nAvailable formats:\r\n";
@@ -54,33 +56,44 @@ namespace WindowsFormsApplication1
 
                 textBox2.Text = mathMl.Replace("><", ">\r\n<");
             }
-          // good, for text
+            // good, for text
             //textBox2.Text = (string)ido.GetData(DataFormats.UnicodeText);
-            }
+        }
 
-            private void button2_Click(object sender, System.EventArgs e)
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            string str1 = "X<?xml version=\"1.0\" encoding=\"UTF-16\"?><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mfrac><mi>a</mi><mi>b</mi></mfrac></math>";
+            Byte[] str2 = System.Text.Encoding.Unicode.GetBytes(str1);
+            str2[0] = 255;
+            str2[1] = 254;
+
+            MemoryStream clp = new MemoryStream(str2);
+            Clipboard.SetData("MathML", clp);
+        }
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            string str0 = textBox2.Text;
+            XmlReader reader = XmlReader.Create(new StringReader(str0));
+            try
             {
-                string str1 = "X<?xml version=\"1.0\" encoding=\"UTF-16\"?><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mfrac><mi>a</mi><mi>b</mi></mfrac></math>";
+                XDocument testdoc = new XDocument(XDocument.Parse(str0));
+
+                string str1 = Regex.Replace(str0, @"^(<\?xml.*\?>)?", "X<?xml version=\"1.0\" encoding=\"UTF-16\"?>");
                 Byte[] str2 = System.Text.Encoding.Unicode.GetBytes(str1);
                 str2[0] = 255;
                 str2[1] = 254;
-
                 MemoryStream clp = new MemoryStream(str2);
                 Clipboard.SetData("MathML", clp);
             }
-            private void button3_Click(object sender, System.EventArgs e)
+            catch (Exception xmlerr)
             {
-                string str0 = textBox2.Text;
-                string str1=Regex.Replace(str0,@"^(<\?xml.*\?>)?","X<?xml version=\"1.0\" encoding=\"UTF-16\"?>");
-                Byte[] str2 = System.Text.Encoding.Unicode.GetBytes(str1);
-                str2[0] = 255;
-                str2[1] = 254;
-                MemoryStream clp = new MemoryStream(str2);
-                Clipboard.SetData("MathML", clp);
+                textBox2.Text = "\r\nXML error Data not copied:\r\n\r\n" + xmlerr.Message + "\r\n-----\r\n" + textBox2.Text;
             }
 
         }
 
-     
     }
+
+
+}
 
